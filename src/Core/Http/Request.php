10d04d8eca22c026cf7d3c4231da58dcef6d851f
restu <restu.suhendar@gmail.com>
2016-05-03 01:44:02 +0700

    add function get ip client to request

--- src/Core/Http/Request.php
+++ src/Core/Http/Request.php
@@ -1,7 +1,23 @@
 <?php
 namespace JayaCode\Framework\Core\Http;
 
-class Request
+class Request extends \Symfony\Component\HttpFoundation\Request
 {
+    /**
+     * Return IP address client
+     * @return string
+     */
+    public function ip()
+    {
+        return $this->getClientIp();
+    }
 
+    /**
+     * Return IP address client
+     * @return array
+     */
+    public function ipAll()
+    {
+        return $this->getClientIps();
+    }
 }
