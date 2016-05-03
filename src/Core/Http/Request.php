6b6722b0c67445cec447cd13e3e6f9366de27925
restu suhendar <restu.suhendar@trans7.co.id>
2016-05-03 12:23:23 +0700

    get path, get root url, get method request

--- src/Core/Http/Request.php
+++ src/Core/Http/Request.php
@@ -3,6 +3,38 @@ namespace JayaCode\Framework\Core\Http;
 
 class Request extends \Symfony\Component\HttpFoundation\Request
 {
+
+    /**
+     * Get the current path info for the request.
+     *
+     * @return string
+     */
+    public function path()
+    {
+        $pattern = trim($this->getPathInfo(), '/');
+        return $pattern == '' ? '/' : $pattern;
+    }
+    
+    /**
+     * Get the request method.
+     *
+     * @return string
+     */
+    public function method()
+    {
+        return $this->getMethod();
+    }
+
+    /**
+     * Get the root URL
+     *
+     * @return string
+     */
+    public function rootURL()
+    {
+        return rtrim($this->getSchemeAndHttpHost().$this->getBaseUrl(), '/');
+    }
+
     /**
      * Return true if server HTTP_REFERER isset
      *
@@ -18,7 +50,7 @@ class Request extends \Symfony\Component\HttpFoundation\Request
      *
      * @return string
      */
-    public function getRefererURL()
+    public function refererURL()
     {
         return $this->server->get("HTTP_REFERER");
     }
--- tests/Core/Http/RequestTest.php
+++ tests/Core/Http/RequestTest.php
@@ -5,6 +5,39 @@ use JayaCode\Framework\Core\Http\Request;
 
 class RequestTest extends \PHPUnit_Framework_TestCase
 {
+    public function testGetPath()
+    {
+        $request = Request::create('', 'GET');
+        $this->assertEquals('/', $request->path());
+
+        $request = Request::create('/indonesia/garut/cikajang/', 'GET');
+        $this->assertEquals('indonesia/garut/cikajang', $request->path());
+    }
+
+    public function testMethodMethod()
+    {
+        $request = Request::create('', 'GET');
+        $this->assertSame('GET', $request->method());
+        $request = Request::create('', 'HEAD');
+        $this->assertSame('HEAD', $request->method());
+        $request = Request::create('', 'POST');
+        $this->assertSame('POST', $request->method());
+        $request = Request::create('', 'PUT');
+        $this->assertSame('PUT', $request->method());
+        $request = Request::create('', 'PATCH');
+        $this->assertSame('PATCH', $request->method());
+        $request = Request::create('', 'DELETE');
+        $this->assertSame('DELETE', $request->method());
+        $request = Request::create('', 'OPTIONS');
+        $this->assertSame('OPTIONS', $request->method());
+    }
+
+    public function testRootURL()
+    {
+        $request = Request::create('http://test.com/garut/cikajang');
+        $this->assertSame('http://test.com', $request->rootURL());
+    }
+
     public function testHasRefererURL()
     {
         $request = new Request();
@@ -19,10 +52,10 @@ class RequestTest extends \PHPUnit_Framework_TestCase
     {
         $request = new Request();
 
-        $this->assertNull($request->getRefererURL());
+        $this->assertNull($request->refererURL());
 
         $request->initialize([], [], [], [], [], ['HTTP_REFERER' => 'http://test.com']);
-        $this->assertEquals('http://test.com', $request->getRefererURL());
+        $this->assertEquals('http://test.com', $request->refererURL());
     }
 
     /**
