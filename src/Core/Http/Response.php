133765f7f4b7905d7de52f4457353cfead880ec9
restu <restu.suhendar@gmail.com>
2016-05-14 19:56:29 +0700

    'not found : ' can contain request data and is used in output context(s) leading to a potential security vulnerability.

--- src/Core/Http/Response.php
+++ src/Core/Http/Response.php
@@ -38,6 +38,5 @@ class Response extends BaseResponse
     public function setNotFound($path)
     {
         $this->setStatusCode(404);
-        $this->setDataContent("not found : {$path}");
     }
 }
