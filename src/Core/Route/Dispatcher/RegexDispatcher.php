816e5a98a05187f2f3572a4a81bc7a61221e453f
restu <restu.suhendar@gmail.com>
2016-05-19 02:33:11 +0700

    split dispatcher route & push complexity

--- src/Core/Route/Dispatcher/RegexDispatcher.php
+++ src/Core/Route/Dispatcher/RegexDispatcher.php
@@ -11,8 +11,14 @@ use JayaCode\Framework\Core\Route\Status;
  */
 class RegexDispatcher implements Dispatcher
 {
+    /**
+     * @var RouteCollector
+     */
     protected $routeCollector;
 
+    /**
+     * @var null
+     */
     protected $data;
     /**
      * @var
@@ -29,6 +35,10 @@ class RegexDispatcher implements Dispatcher
      */
     protected $httpMethod;
 
+    /**
+     * @param RouteCollector $routeCollector
+     * @param null $data
+     */
     public function __construct(RouteCollector $routeCollector, $data = null)
     {
         $this->routeCollector = $routeCollector;
@@ -45,46 +55,78 @@ class RegexDispatcher implements Dispatcher
         $this->setPath($path);
         $this->httpMethod = $httpMethod;
 
+        $data = is_null($this->data)?$this->routeCollector->getData():$this->data;
+        return $this->findDataFromAllRoute($data);
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function findDataFromAllRoute(array $data)
+    {
         $result = [
             Status::NOT_FOUND
         ];
 
-        $data = is_null($this->data)?$this->routeCollector->getData():$this->data;
         $countData = count($data);
         $countExplodePath = count($this->explodedPath);
 
-        $i = 0;
-        while ($result[0] == Status::NOT_FOUND && $countData > $i) {
-            /** @var Route $route */
-            $route = $data[$i];
-
-            if ($this->httpMethod == $route->method && $countExplodePath == count($route->explodePath)) {
-                $result[0] = Status::FOUND;
-                if ($this->path != $route->path) {
-                    for ($j = 0; $j < $countExplodePath && $result[0] == Status::FOUND; $j++) {
-                        $countMatches = count($route->ruleMatches[$j]);
-
-                        if ($countMatches == 0 ||
-                            $countMatches == 2 && $this->explodedPath[$j] != $route->explodePath[$j] ||
-                            $countMatches == 5 &&
-                            !preg_match("/^".$route->ruleMatches[$j][4]."$/", $this->explodedPath[$j])) {
-                            $result[0] = Status::NOT_FOUND;
-                        } elseif ($countMatches == 3 || $countMatches == 5) {
-                            $result[2][$route->ruleMatches[$j][2]] = $this->explodedPath[$j];
-                        }
-                    }
-                }
-
-
-                if ($result[0] == Status::FOUND) {
-                    $result[1] = $route->handle;
-                }
-            }
-
-            $i++;
+        for ($i=0; $result[0] == Status::NOT_FOUND && $countData > $i; $i++) {
+            $this->findDataByRouteMethodAndRulePath($data[$i], $countExplodePath, $result);
         }
 
         return $result;
+    }
+
+    /**
+     * @param Route $route
+     * @param $countExplodePath
+     * @param array $result
+     */
+    protected function findDataByRouteMethodAndRulePath(Route $route, $countExplodePath, array &$result)
+    {
+        if ($this->httpMethod == $route->method && $countExplodePath == count($route->explodePath)) {
+            $this->findDataByAllMatchesRulePath($route, $countExplodePath, $result);
+
+            if ($result[0] == Status::FOUND) {
+                $result[1] = $route->handle;
+            }
+        }
+    }
+
+    /**
+     * @param Route $route
+     * @param $countExplodePath
+     * @param array $result
+     */
+    protected function findDataByAllMatchesRulePath(Route $route, $countExplodePath, array &$result)
+    {
+        $result[0] = Status::FOUND;
+        if ($this->path != $route->path) {
+            for ($j = 0; $j < $countExplodePath && $result[0] == Status::FOUND; $j++) {
+                $this->findDataByMatchesRulePath($route, $j, $result);
+            }
+        }
+    }
+
+    /**
+     * @param Route $route
+     * @param $index
+     * @param array $result
+     */
+    protected function findDataByMatchesRulePath(Route $route, $index, array &$result)
+    {
+        $countMatches = count($route->ruleMatches[$index]);
+
+        if ($countMatches == 0 ||
+            $countMatches == 2 && $this->explodedPath[$index] != $route->explodePath[$index] ||
+            $countMatches == 5 &&
+            !preg_match("/^".$route->ruleMatches[$index][4]."$/", $this->explodedPath[$index])) {
+            $result[0] = Status::NOT_FOUND;
+        } elseif ($countMatches == 3 || $countMatches == 5) {
+            $result[2][$route->ruleMatches[$index][2]] = $this->explodedPath[$index];
+        }
     }
 
     /**
